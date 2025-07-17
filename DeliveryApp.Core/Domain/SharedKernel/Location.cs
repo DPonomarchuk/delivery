caa128e06da40207f523ad142fd9a9c8f8351484
Пономарчук Дмитрий <dponomarchuk@softrust.ru>
2025-07-17 23:57:41 +0300

    Добавил класс Location.cs, соответствующий паттерну ValueObject. Покрыл класс Unit Test'ами.

---
+++ DeliveryApp.Core/Domain/SharedKernel/Location.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel;
+
+/// <summary>
+/// Координата на доске.
+/// </summary>
+public class Location : ValueObject
+{
+    /// <summary>
+    ///     Конструктор
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    private Location() { }
+
+    /// <summary>
+    ///     Конструктор
+    /// </summary>
+    /// <param name="x">Горизонталь</param>
+    /// <param name="y">Вертикаль</param>
+    private Location(short x, short y) : this()
+    {
+        X = x;
+        Y = y;
+    }
+
+    /// <summary>
+    /// Координата по горизонтали
+    /// </summary>
+    public short X { get; }
+
+    /// <summary>
+    /// Координата по вертикали
+    /// </summary>
+    public short Y { get; }
+
+    /// <summary>
+    /// Создание координаты по заданным параметрам
+    /// </summary>
+    /// <param name="x">Значение по горизонтали</param>
+    /// <param name="y">Значение по вертикали</param>
+    /// <returns></returns>
+    public static Result<Location, Error> Create(short x, short y)
+    {
+        if (x <= 0) return GeneralErrors.ValueIsRequired(nameof(x));
+        if (y <= 0) return GeneralErrors.ValueIsRequired(nameof(y));
+        if (x > 10) return GeneralErrors.ValueIsInvalid(nameof(x));
+        if (y > 10) return GeneralErrors.ValueIsInvalid(nameof(y));
+        return new Location(x, y);
+    }
+
+    /// <summary>
+    /// Создание координаты со случайными значениями
+    /// </summary>
+    /// <returns></returns>
+    public static Result<Location, Error> CreateRandom()
+    {
+        var random = new Random();
+        return Create((short)random.Next(1, 10), (short)random.Next(1, 10));
+    }
+
+    /// <summary>
+    /// Вычислить расстояние до другой координаты
+    /// </summary>
+    /// <param name="other">Координата</param>
+    /// <returns>Расстояние</returns>
+    public short CalculateDistance(Location other)
+    {
+        return (short)(Math.Abs(X - other.X) + Math.Abs(Y - other.Y));
+    }
+
+    protected bool Equals(Location other)
+    {
+        return base.Equals(other) && X == other.X && Y == other.Y;
+    }
+
+    public static bool operator ==(Location left, Location right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Location left, Location right)
+    {
+        return !(left == right);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return X;
+        yield return Y;
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/SharedKernel/LocationShould.cs
@@ -0,0 +1,100 @@
+﻿using DeliveryApp.Core.Domain.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.SharedKernel;
+
+public class LocationShould
+{
+    [Fact]
+    public void BeCorrectWhenParamsIsCorrectOnCreate()
+    {
+        //Arrange
+
+        //Act
+        var location = Location.Create(1, 3);
+
+        //Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(1);
+        location.Value.Y.Should().Be(3);
+    }
+
+    [Theory]
+    [InlineData(-1, 3)]
+    [InlineData(2, 0)]
+    [InlineData(-2, -8)]
+    [InlineData(11, 8)]
+    [InlineData(8, 11)]
+    public void ReturnErrorWhenParamsIncorrectOnCreate(short x, short y)
+    {
+        //Arrange
+
+        //Act
+        var location = Location.Create(x, y);
+
+        //Assert
+        location.IsSuccess.Should().BeFalse();
+        location.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void BeEqualWhenParamsAreEqual()
+    {
+        //Arrange
+        var location1 = Location.Create(1, 3);
+        var location2 = Location.Create(1, 3);
+
+        //Act
+        var result = location1.Value == location2.Value;
+
+        //Assert
+        result.Should().BeTrue();
+        location1.Value.Equals(location2.Value).Should().BeTrue();
+    }
+
+    [Fact]
+    public void BeNotEqualWhenAllParamsAreEqual()
+    {
+        //Arrange
+        var location1 = Location.Create(1, 3);
+        var location2 = Location.Create(1, 3);
+
+        //Act
+        var result = location1.Value != location2.Value;
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void BeCorrectWhenCreateRandom()
+    {
+        //Arrange
+
+        //Act
+        var location = Location.CreateRandom();
+
+        //Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().BeGreaterThan(0).And.BeLessThan(11);
+        location.Value.Y.Should().BeGreaterThan(0).And.BeLessThan(11);
+    }
+
+    [Theory]
+    [InlineData(1, 3, 2)]
+    [InlineData(4, 8, 10)]
+    [InlineData(9, 9, 16)]
+    public void BeCorrectWhenCalculateDistance(short x, short y, short distance)
+    {
+        //Arrange
+        var start = Location.Create(1, 1);
+        var finish = Location.Create(x, y);
+
+        //Act
+        var calculatedDistance = start.Value.CalculateDistance(finish.Value);
+
+        //Assert
+        calculatedDistance.Should().Be(distance);
+    }
+}
