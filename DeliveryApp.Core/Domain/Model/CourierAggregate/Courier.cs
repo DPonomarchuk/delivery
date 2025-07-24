ff8eeef60f6ee68467d55c9518bbe31f20883572
Пономарчук Дмитрий <dponomarchuk@softrust.ru>
2025-07-24 21:05:44 +0300

    Добавил модели заказа и статуса заказа

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
@@ -0,0 +1,36 @@
+﻿using DeliveryApp.Core.Domain.SharedKernel;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate;
+
+public class Courier : Aggregate<Guid>
+{
+    private Courier()
+    {
+    }
+
+    private Courier(string name, int speed)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Speed = speed;
+    }
+
+    /// <summary>
+    ///     Имя
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    ///     Скорость
+    /// </summary>
+    public int Speed { get; private set; }
+
+    /// <summary>
+    ///     Местоположение
+    /// </summary>
+    public Location Location { get; private set; }
+
+
+
+}
