174322d7e574973d9a3bac562a344752e298caf2
Пономарчук Дмитрий <dponomarchuk@softrust.ru>
2025-07-26 14:26:52 +0300

    Заполняю курьера, добавил тесты

--- DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
@@ -1,4 +1,7 @@
-﻿using DeliveryApp.Core.Domain.SharedKernel;
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Domain.SharedKernel;
 using Primitives;
 
 namespace DeliveryApp.Core.Domain.Model.CourierAggregate;
@@ -9,11 +12,51 @@ public class Courier : Aggregate<Guid>
     {
     }
 
-    private Courier(string name, int speed)
+    private Courier(string name, int speed, Location location)
     {
         Id = Guid.NewGuid();
         Name = name;
         Speed = speed;
+        Location = location;
+    }
+
+    /// <summary>
+    ///     Создание курьера
+    /// </summary>
+    /// <param name="name">Имя</param>
+    /// <param name="speed">Скорость</param>
+    /// <param name="location">Расположение</param>
+    /// <returns></returns>
+    public static Result<Courier, Error> Create(string name, int speed, Location location)
+    {
+        // validations
+        var courier = new Courier(name, speed, location);
+
+        var bag = StoragePlace.Create("Сумка", 10).Value;
+        courier.AddStoragePlace(bag);
+        return courier;
+    }
+
+    /// <summary>
+    /// Добавления места хранения
+    /// </summary>
+    /// <param name="storagePlace">Место хранения</param>
+    /// <returns>Результат операции</returns>
+    public UnitResult<Error> AddStoragePlace(StoragePlace storagePlace)
+    {
+        StoragePlaces.Add(storagePlace);
+        return UnitResult.Success<Error>();
+    }
+
+    /// <summary>
+    /// Проверить возможность взятия заказа.
+    /// </summary>
+    /// <param name="order">Заказ</param>
+    /// <returns></returns>
+    public Result<bool, Error> CanTakeOrder(Order order)
+    {
+        if (StoragePlaces is null || StoragePlaces.Count < 1) return Errors.NoStoragePlaces();
+        return StoragePlaces.Any(x => x.TotalVolume >= order.Volume);
     }
 
     /// <summary>
@@ -31,6 +74,21 @@ public class Courier : Aggregate<Guid>
     /// </summary>
     public Location Location { get; private set; }
 
+    /// <summary>
+    /// Места для хранения
+    /// </summary>
+    public List<StoragePlace> StoragePlaces { get; private set; }
 
-
+    /// <summary>
+    ///     Ошибки, которые может возвращать сущность
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class Errors
+    {
+        public static Error NoStoragePlaces()
+        {
+            return new Error($"{nameof(StoragePlaces).ToLowerInvariant()}.cant.take.order",
+                "Невозможно взять заказ, нет доступных мест для хранения");
+        }
+    }
 }
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/CourierAggregate/CourierShould.cs
@@ -0,0 +1,25 @@
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.CourierAggregate;
+
+public class CourierShould
+{
+    [Fact]
+    public void BeCorrectWhenParamsIsCorrectOnCreate()
+    {
+        //Arrange
+        var location = Location.CreateRandom();
+        //Act
+        var courier = Courier.Create("Ваня", 10, location);
+
+        //Assert
+        courier.IsSuccess.Should().BeTrue();
+        courier.Value.Name.Should().Be("Ваня");
+        courier.Value.Speed.Should().Be(10);
+        courier.Value.Location.Should().NotBeNull();
+        courier.Value.StoragePlaces.Should().NotBeNull();
+    }
+}
