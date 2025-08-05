30cd68097de7f2a74e1b2c72cdfb9e61a120aae9
Пономарчук Дмитрий <dponomarchuk@softrust.ru>
2025-08-05 22:27:32 +0300

    Добавил репозиторий для работы с Курьером. Сгенерировал миграцию. Добавил конфигурацию EFCore для настройки агрегата курьера. Покрыл репозиторий интеграционными тестами. Убрал лишние gitkeep

---
+++ DeliveryApp.Core/Ports/ICourierRepository.cs
@@ -0,0 +1,38 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using Primitives;
+
+namespace DeliveryApp.Core.Ports;
+
+/// <summary>
+///     Repository для Aggregate OrderDTO
+/// </summary>
+public interface ICourierRepository : IRepository<Courier>
+{
+    /// <summary>
+    ///     Добавить
+    /// </summary>
+    /// <param name="courier">Заказ</param>
+    /// <returns>Заказ</returns>
+    Task AddAsync(Courier courier);
+
+    /// <summary>
+    ///     Обновить
+    /// </summary>
+    /// <param name="courier">Заказ</param>
+    void Update(Courier courier);
+
+    /// <summary>
+    ///     Получить
+    /// </summary>
+    /// <param name="courierId">Идентификатор</param>
+    /// <returns>Заказ</returns>
+    Task<Maybe<Courier>> GetAsync(Guid courierId);
+
+    /// <summary>
+    ///     Получить всех свободных курьеров
+    /// </summary>
+    /// <returns>Курьеры</returns>
+    IEnumerable<Courier> GetAllFree();
+}
---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/CourierRepository.cs
@@ -0,0 +1,43 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
+
+public class CourierRepository : ICourierRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CourierRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task AddAsync(Courier courier)
+    {
+        await _dbContext.Couriers.AddAsync(courier);
+    }
+
+    public void Update(Courier courier)
+    {
+        _dbContext.Couriers.Update(courier);
+    }
+
+    public async Task<Maybe<Courier>> GetAsync(Guid courierId)
+    {
+        var courier = await _dbContext
+            .Couriers
+            .Include(c => c.StoragePlaces)
+            .SingleOrDefaultAsync(o => o.Id == courierId);
+        return courier;
+    }
+
+    public IEnumerable<Courier> GetAllFree()
+    {
+        var couriers = _dbContext.Couriers.Include(c => c.StoragePlaces)
+            .Where(c => c.StoragePlaces.All(x => x.OrderId == null));
+        return couriers;
+    }
+}
